ab924b38fa448035e75c22417f917bcc00763fbb
PiotrCzlapinski <piotr1cz@wp.pl>
2021-12-18 10:30:48 +0100

    Plansza do gry skończona

--- main.py
+++ main.py
@@ -1,5 +1,45 @@
-# Tu będzie gra kółko i krzyżyk. gra nr 1
+
+#%%
+# # Tu będzie gra kółko i krzyżyk. gra nr 1
 
 PlanszaDoGry = {'7':' ', "8": ' ' , '9':' ',
                 '4':' ', "5": ' ' , '6':' ',
                 '1':' ', "2":' ' , '3':' '}
+klawiszeGry=[]
+
+for key in PlanszaDoGry:
+    klawiszeGry.append(key)
+
+def drukujPlansze():
+    print(' ' + '|' + ' ' + '|' + ' ')
+    print('-+-+-')
+    print(' ' + '|' + ' ' + '|' + ' ')
+    print('-+-+-')
+    print(' ' + '|' + ' ' + '|' + ' ')
+   
+    #print(klawiszeGry)
+drukujPlansze()
+
+#%%
+#%%
+# # Tu będzie gra kółko i krzyżyk. gra nr 1 rozinięcie 
+
+PlanszaDoGry = {'7':' ', "8": ' ' , '9':' ',
+                '4':' ', "5": ' ' , '6':' ',
+                '1':' ', "2":' ' , '3':' '}
+klawiszeGry=[]
+
+for key in PlanszaDoGry:
+    klawiszeGry.append(key)
+
+def drukujPlansze(pole):
+    print(f"{pole['7']}|{pole['8']}|{pole['9']}")
+    print('-+-+-')
+    print(f"{pole['4']}|{pole['5']}|{pole['6']}")
+    print('-+-+-')
+    print(f"{pole['1']}|{pole['2']}|{pole['3']}")
+   
+#print(klawiszeGry)
+drukujPlansze(PlanszaDoGry)
+
+# %%
